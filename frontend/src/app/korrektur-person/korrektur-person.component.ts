a1c5bdf3374cd03d9dcd6f24c45e2b981bce214b
MartinWenge <wengenmayr.martin@outlook.de>
2025-10-21 20:25:11 +0200

    implementation delete connection workflow in frontend

--- frontend/src/app/korrektur-person/korrektur-person.component.ts
+++ frontend/src/app/korrektur-person/korrektur-person.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Person, PersonConnection } from '../models/person';
-import { HttpClient } from '@angular/common/http';
+import { Person, PersonConnection, PersonMitVerbindungen, PersonenVerbinden } from '../models/person';
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { debounceTime, distinctUntilChanged, map, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs';
 import { ApiConfigService } from '../services/api-config.service';
@@ -15,21 +15,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class KorrekturPersonComponent implements OnInit, OnDestroy {
   allePersonen: Person[] = [];
+  connectionsErstePerson: Person[] = [];
   suchErgebnisse: Person[] = [];
+  suchErgebnis1: Person[] = [];
+  suchErgebnis2: Person[] = [];
   ausgewaehltePerson: Person | null = null;
+  gefundenePerson1: Person | null = null;
+  gefundenePerson2: Person | null = null;
   loeschenForm: FormGroup;
+  loeseVerbindungForm: FormGroup;
   private destroy$ = new Subject<void>();
   isLoading: boolean = false;
   selectedCorrectionType: string = "";
-  apiUrlPersonLoeschen: string = "api/deleteperson"
+  apiUrlPersonLoeschen: string = "api/deleteperson";
+  apiUrlPersonZumSuchem: string = "api/verbindungen";
+  apiUrlVerbindungZumLoesen: string = "api/deleteverbindung";
   erfolgsmeldung: string = '';
   fehlermeldung: string = '';
   personIstGeloescht: boolean = false;
+  verbindungIstGeloest: boolean = false;
 
   constructor(private fb: FormBuilder, private http: HttpClient, private apiConfig: ApiConfigService, private personenBereitstellen: PersonenlisteBereitstellenService) {
     this.loeschenForm = this.fb.group({
       personLoeschen: ['', Validators.required],
-    })
+    }),
+      this.loeseVerbindungForm = this.fb.group({
+        person1: ['', Validators.required],
+        person2: ['', Validators.required]
+      })
   }
 
   onCorrectionTypeChange(event: any | undefined): void {
@@ -42,12 +55,19 @@ export class KorrekturPersonComponent implements OnInit, OnDestroy {
     this.personIstGeloescht = false;
   }
 
+  resetverbindungIstGeloest(): void {
+    this.verbindungIstGeloest = false;
+  }
+
   ngOnInit(): void {
     this.personenBereitstellen.getAllePersonen()
       .pipe(takeUntil(this.destroy$))
       .subscribe(personen => {
         this.allePersonen = personen;
+        this.connectionsErstePerson = personen;
         this.setupSuche();
+        this.setupSuche1();
+        this.setupSuche2();
       });
   }
 
@@ -69,9 +89,40 @@ export class KorrekturPersonComponent implements OnInit, OnDestroy {
       });
   }
 
+  setupSuche1(): void {
+    this.loeseVerbindungForm.controls['person1'].valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$),
+        map(term => term ? this.filterPersonen(term) : [])
+      ).subscribe(ergebnisse => {
+        this.suchErgebnis1 = ergebnisse
+      })
+  }
+
+  setupSuche2(): void {
+    this.loeseVerbindungForm.controls['person2'].valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$),
+        map(term => term ? this.filterPerson2(term) : [])
+      ).subscribe(ergebnisse => {
+        this.suchErgebnis2 = ergebnisse
+      })
+  }
+
   filterPersonen(term: string): Person[] {
     term = term.toLowerCase();
     return this.allePersonen.filter(person =>
+      person.vorname.toLowerCase().includes(term) || person.nachname.toLowerCase().includes(term)
+    );
+  }
+
+  filterPerson2(term: string): Person[] {
+    term = term.toLowerCase();
+    return this.connectionsErstePerson.filter(person =>
       person.vorname.toLowerCase().includes(term) || person.nachname.toLowerCase().includes(term)
     );
   }
@@ -83,6 +134,20 @@ export class KorrekturPersonComponent implements OnInit, OnDestroy {
     }
   }
 
+  onInputChange1(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      const inputValue = event.target.value;
+      this.loeseVerbindungForm.controls['person1'].setValue(inputValue);
+    }
+  }
+
+  onInputChange2(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      const inputValue = event.target.value;
+      this.loeseVerbindungForm.controls['person2'].setValue(inputValue);
+    }
+  }
+
   selectPerson(person: Person): void {
     this.ausgewaehltePerson = person;
     this.loeschenForm.patchValue({
@@ -91,12 +156,87 @@ export class KorrekturPersonComponent implements OnInit, OnDestroy {
     this.suchErgebnisse = [];
   }
 
-  entferneVerbindung(): void {
+  selectPerson1(person: Person): void {
+    this.gefundenePerson1 = person;
+    this.loeseVerbindungForm.patchValue({
+      person1: `${person.vorname} ${person.nachname}, ${new Date(person.geburtstag).toLocaleDateString()}`
+    });
+    this.suchErgebnis1 = [];
+
+    let parameter = new HttpParams();
+    if (this.gefundenePerson1?.vorname) {
+      parameter = parameter.set('vorname', this.gefundenePerson1.vorname);
+    }
+    if (this.gefundenePerson1?.nachname) {
+      parameter = parameter.set('nachname', this.gefundenePerson1.nachname);
+    }
+    if (this.gefundenePerson1?.geburtstag) {
+      parameter = parameter.set('geburtstag', String(this.gefundenePerson1.geburtstag));
+    }
+    if (this.gefundenePerson1?.tenant) {
+      parameter = parameter.set('tenant', this.gefundenePerson1.tenant);
+    }
+    this.http.get<PersonMitVerbindungen>(this.apiConfig.apiUrl + this.apiUrlPersonZumSuchem, { params: parameter })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          console.log("Erfolgreich Verbindungen zu Person abgerufen");
+          this.fehlermeldung = ''
+          this.connectionsErstePerson = [
+            ...(response.ehepartner || []),
+            ...(response.eltern || []),
+            ...(response.kinder || [])
+          ];
+          this.isLoading = false;
+        },
+        error: (error: HttpErrorResponse) => {
+          let errorMessage = 'Ein unbekannter Fehler ist aufgetreten.';
+
+          if (error.error instanceof ErrorEvent) {
+            errorMessage = `Fehler: ${error.error.message}`;
+          } else {
+            switch (error.status) {
+              case 422:
+                errorMessage = 'Die Eingabeparameter konnten nicht verarbeitet werden';
+                break;
+              case 500:
+                errorMessage = 'Ein unerwarteter Serverfehler ist aufgetreten.';
+                break;
+              default:
+                errorMessage = `Unbekannter Fehler ${error.status}: Ein Problem ist aufgetreten.`;
+                break;
+            }
+          }
+          this.fehlermeldung = errorMessage;
+          this.erfolgsmeldung = '';
+          this.isLoading = false;
+        }
+      });
+
+  }
+
+  selectPerson2(person: Person): void {
+    this.gefundenePerson2 = person;
+    this.loeseVerbindungForm.patchValue({
+      person2: `${person.vorname} ${person.nachname}, ${new Date(person.geburtstag).toLocaleDateString()}`
+    });
+    this.suchErgebnis2 = [];
+  }
+
+  setzeGefundenePersonZurueck(): void {
     this.ausgewaehltePerson = null;
     this.loeschenForm.patchValue({ personLoeschen: null });
   }
 
-  onSubmit(): void {
+  setzeGefundenePersonenZurueck(): void {
+    this.gefundenePerson1 = null;
+    this.gefundenePerson2 = null;
+    this.connectionsErstePerson = [];
+    this.loeseVerbindungForm.patchValue({ person1: null });
+    this.loeseVerbindungForm.patchValue({ person2: null });
+  }
+
+  onSubmitLoeschePerson(): void {
     if (this.loeschenForm.valid) {
       this.isLoading = true;
 
@@ -131,7 +271,59 @@ export class KorrekturPersonComponent implements OnInit, OnDestroy {
           }
         });
     } else {
-      this.fehlermeldung = 'Bitte füllen Sie alle erforderlichen Felder aus.';
+      this.fehlermeldung = 'Bitte fülle alle erforderlichen Felder aus.';
+      this.erfolgsmeldung = '';
+    }
+  }
+
+  onSubmitLoeseVerbindung(): void {
+    if (this.loeseVerbindungForm.valid) {
+      this.isLoading = true;
+      const verbindungZumLoesen: PersonenVerbinden = {
+        person1: {
+          vorname: this.gefundenePerson1?.vorname,
+          nachname: this.gefundenePerson1?.nachname,
+          geburtstag: this.gefundenePerson1?.geburtstag,
+          tenant: this.gefundenePerson1?.tenant
+        },
+        person2: {
+          vorname: this.gefundenePerson2?.vorname,
+          nachname: this.gefundenePerson2?.nachname,
+          geburtstag: this.gefundenePerson2?.geburtstag,
+          tenant: this.gefundenePerson2?.tenant
+        },
+        verbindungsart: "loeschen"
+      }
+
+      this.http.post<PersonenVerbinden>(this.apiConfig.apiUrl + this.apiUrlVerbindungZumLoesen, verbindungZumLoesen)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            this.erfolgsmeldung = `${this.gefundenePerson1?.vorname} ist jetzt nicht mehr mit ${this.gefundenePerson2?.vorname} verbunden`;
+            this.fehlermeldung = '';
+            this.loeseVerbindungForm.reset();
+            this.gefundenePerson1 = null;
+            this.gefundenePerson2 = null;
+            this.personenBereitstellen.invalidateCache();
+            this.personenBereitstellen.getAllePersonen().subscribe(updatedPersonen => {
+              this.allePersonen = updatedPersonen;
+              this.setupSuche();
+              this.setupSuche1();
+              this.setupSuche2();
+              this.verbindungIstGeloest = true;
+              this.isLoading = false;
+            });
+          },
+          error: (error) => {
+            console.error('Fehler beim Lösen der Verbindung')
+            this.fehlermeldung = 'Fehler beim Lösen der Verbindung';
+            this.erfolgsmeldung = '';
+            this.isLoading = false;
+          }
+        });
+
+    } else {
+      this.fehlermeldung = 'Bitte fülle alle erforderlichen Felder aus.';
       this.erfolgsmeldung = '';
     }
   }
